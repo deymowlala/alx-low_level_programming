ea9a4cb1e0cac2a3809b0a7689b6efe8cc60b614
deymowlala <office.ademola@gmail.com>
2023-08-13 08:04:05 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints al single-digit numbers with ,
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar((n % 10) + '0');
+		if (n < 9)
+			putchar(',');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
