b7cf0d96083256f7ef6cdb83bc904cdd12e7aa38
deymowlala <office.ademola@gmail.com>
2023-08-16 16:43:02 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,10 @@ void print_alphabet_x10(void)
 	char alphabet = 'a';
 	int i;
 
-        for(i=0; i <10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		while(alphabet <= 'z')
-		{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
-			alphabet++;
-		}
 		_putchar('\n');
 	}
-	return (0);
 }
