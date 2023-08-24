1344bcaa2934173e067021b8b1be6cd9405f8534
deymowlala <office.ademola@gmail.com>
2023-08-23 23:18:19 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strncat - Function that concatenates two strings
+ *@dest: String to be added to
+ @src: String to concatenate to dest
+ @n: Total number of character to copy to dest from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int leng = strlen(dest);
+	int j;
+
+	for (j = 0; j < n && *src != '\0'; j++)
+	{
+		dest[leng + j] = src[j];
+		src++;
+	}
+	return (dest);
+}
