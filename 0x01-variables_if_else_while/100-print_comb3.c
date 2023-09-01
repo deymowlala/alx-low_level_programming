e07dcae6798dac2f7550d9eda76e234855ba9436
deymowlala <office.ademola@gmail.com>
2023-09-01 10:12:13 -0700

    for 0x07

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,22 @@
 
 int main(void)
 {
-	int fig1, fig2;
+	int fig1 = '0'; 
+	int fig2 = '0';
 
-	for (fig1 = 0; fig1 < 9; fig1++)
+	for (fig2 = 0; fig2 <= 9; fig2++)
 	{
-		for (fig2 = 1; fig2 < 10; fig2++)
+		for (fig1 = 0; fig1 <= 8; fig1++)
 		{
-			if (fig1 != fig2)
+			if (!((fig1 == fig2) || (fig2 > fig1)))
 			{
-				putchar((fig1 % 10) + '0');
-				putchar((fig2 % 10) + '0');
-				putchar(',');
-				putchar(' ');
+				putchar(fig2);
+				putchar(fig1);
+				if (!(fig1 == '9') && (fig2 == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
---
+++ README.md
@@ -0,0 +1,10 @@
+0x00. C - Hello, World
+0x01-variables_if_else_while
+0x02-functions_nested_loops
+0x03-debugging
+0x04
+0x05. C - Pointers, arrays and strings
+0x06. C - More pointers, arrays and strings
+0x08. C - Recursion
+0x09. C - Static libraries
+
