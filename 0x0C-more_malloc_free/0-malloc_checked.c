69226e06b166e42fc757b6226d89ff5e0dfc96ea
deymowlala <office.ademola@gmail.com>
2023-09-08 14:20:25 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Function that allocates memory using malloc
+ * @b: Size of memory
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *result;
+	
+	result = malloc(b);
+	if (result == NULL)
+		exit(98);
+	else
+		return (result);
+}
