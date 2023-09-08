d9f99cd35036fe37d176e038f4f65230c80af267
deymowlala <office.ademola@gmail.com>
2023-09-08 15:16:30 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * malloc_checked - Function that allocates memory using malloc
  * @b: Size of memory
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
 	char *result;
