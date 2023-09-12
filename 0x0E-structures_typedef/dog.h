6d570ffcfc8aeb7abc42c53cc130b7a54f3470b0
deymowlala <office.ademola@gmail.com>
2023-09-12 12:02:00 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog structure
+ * @name:Dog's name
+ * @age: Dog's age
+ * @owner: Owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
