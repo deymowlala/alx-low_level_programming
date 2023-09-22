0808f512509e65259d30d041c6386106c86b9584
deymowlala <office.ademola@gmail.com>
2023-09-22 14:48:44 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds node to the end of the list
+ * @head: Head of the node
+ * @str: String
+ * Return: Address of new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
